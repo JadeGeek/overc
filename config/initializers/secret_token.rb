ffebc101e7374296a54fcf2e2f6c4630fbbae34d
Jaden <malcolm-ding@hotmail.com>
2013-07-13 05:38:03 +1200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Overc::Application.config.secret_token = '2e5975fc1b987a88b893a5cc6b0ecb70dd390a6987e69c2c3896855e0a92065599276ea7b5905692d2270e7382e98060cf42341faa4a2dbde8a87651b3e788ed'
